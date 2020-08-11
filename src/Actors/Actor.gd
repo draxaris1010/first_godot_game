83d381cb51f95aab6a485ec8e6339c33ce16b8d5
Axel <axel.dezittere@gmail.com>
2020-08-11 18:10:14 +0200

    gravity tweaked

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -4,7 +4,7 @@ class_name Actor
 var FLOOR_NORMAL: = Vector2.UP
 
 export var speed: = Vector2(300.0, 1000.0)
-export var gravity: = 3000.0
+export var gravity: = 4000.0
 export var velocity: = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
